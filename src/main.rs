fc174fa1df089f01df7385c9c606e5054732a239
Guilliam Xavier <guilliamxavier@users.noreply.github.com>
2023-11-02 12:15:07 +0100

    simplify initial code

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,13 @@ use std::env;
 // Available if you need it!
 // use serde_bencode
 
-// #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> Value {
-    // If encoded_value starts with a digit, it's a string
     if encoded_value.chars().next().unwrap().is_ascii_digit() {
         // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let length_string = &encoded_value[..colon_index];
-        let length = length_string.parse::<usize>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + length];
-        Value::String(string.to_string())
+        let (len, rest) = encoded_value.split_once(':').unwrap();
+        let len = len.parse::<usize>().unwrap();
+        let string = &rest[..len];
+        Value::String(string.into())
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
@@ -25,14 +22,10 @@ fn main() {
     let command = &args[1];
 
     if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        eprintln!("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
         let decoded_value = decode_bencoded_value(encoded_value);
         println!("{}", decoded_value);
     } else {
-        println!("unknown command: {}", args[1])
+        println!("unknown command: {}", command)
     }
 }
